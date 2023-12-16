f35a05620aee5ad1ac4967692db4e50c17ca9b71
윤용식 <comnori@gmail.com>
2023-12-17 01:54:27 +0900

    Update devcontainer.json, init.sh, and zsh-in-docker.sh

---
+++ .devcontainer/scripts/init.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.5/zsh-in-docker.sh)" \
+  -t jonathan \
+  -p git \
+  -p ssh-agent \
+  -p https://github.com/zsh-users/zsh-autosuggestions \
+  -p https://github.com/zsh-users/zsh-completions
